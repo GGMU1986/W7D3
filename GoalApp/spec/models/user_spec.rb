12297c666113288297bbd4181737403e3d48f78d
gtsimis <gt2186@gmail.com>
2021-09-22 16:06:39 -0400

    Added some stuff

---
+++ GoalApp/spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe User, type: :model do
+
+  subject(:sherlock) { User.create(
+      username: "sherlock",
+      password: "password123")
+  }
+
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_length_of(:password).is_at_least(6) }
+  it { should validate_presence_of(:session_token) }
+  it { should validate_uniqueness_of(:username) }
+end
