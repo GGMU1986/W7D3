4f2f38a963e1e271bf59d3c3c73912d46825311c
gtsimis <gt2186@gmail.com>
2021-09-22 18:20:22 -0400

    Added validation tests

--- GoalApp/app/models/user.rb
+++ GoalApp/app/models/user.rb
@@ -3,10 +3,32 @@ class User < ApplicationRecord
   validates :username, :session_token, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
 
+  after_initialize :ensure_session_token
+
   attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    user && user.is_password?(password) ? user : nil
+  end
+
+  def is_password?(password)
+    password_obj = BCrypt::Password.new(password_digest)
+    password_obj.is_password?(password)
+  end
 
   def password=(password)
     self.password_digest = BCrypt::Password.create(password)
     @password = password
+  end
+
+  def reset_session_token
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64
   end
 end
--- GoalApp/spec/models/user_spec.rb
+++ GoalApp/spec/models/user_spec.rb
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-=======
 require 'rails_helper'
 
->>>>>>> 93406204c0782319340e19e16df35b6527e0d491
 RSpec.describe User, type: :model do
 
   subject(:sherlock) { User.create(
@@ -14,5 +11,33 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:password_digest) }
   it { should validate_length_of(:password).is_at_least(6) }
   it { should validate_presence_of(:session_token) }
-  it { should validate_uniqueness_of(:username) }
+#   it { should validate_uniqueness_of(:username) }
+
+    describe 'uniqueness' do
+        before :each do 
+            create(:user)
+        end
+        it { should validate_uniqueness_of(:username) }
+        it { should validate_uniqueness_of(:session_token) }
+    end
+
+#   describe "find by credentials" do
+#     before {sherlock.save}
+#     context "with valid credentials" do
+#         it "should return user" do
+#             expect(User.find_by_credentials('sherlock', 'password123')).to eq(sherlock)
+#         end
+#     end
+
+#     context "with invalid credentials" do
+#         it "should return nil" do
+#             expect(User.find_by_credentials('sherlok', 'password')).to be(nil)
+#         end
+#     end
+#   end
+
+#   describe "is_password?" do
+#     context
+#   end
+  
 end
